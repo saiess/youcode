22ffe293d288a62acfb21302781e497deec74a9e
saiess <saidessalami0@gmail.com>
2021-01-05 17:43:54 +0100

    new c files

---
+++ c2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,x,i,;
+    printf("entre un nombre*\n");
+    scanf(%d,&n)
+    x=1
+    for i<=20,n*x,x++; 
+   
+
+    return 0;
+}
